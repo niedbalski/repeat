65214772ff190f9ffa5da88cb43ccb9136ff4dfe
Jorge Niedbalski <jnr@metaklass.org>
2020-07-16 23:28:05 -0400

    improve pandas example and library
    Signed-off-by: Jorge Niedbalski <jnr@metaklass.org>

--- repeat_pandas.py
+++ repeat_pandas.py
@@ -9,7 +9,10 @@ import glob
 DEFAULT_COLLECTIONS_FILE = "collections.db"
 
 
-def load_collection(filepath, collection):
+class Collections: pass
+
+
+def load(filepath):
     with tempfile.TemporaryDirectory() as tmp_dir:
         with tarfile.open(filepath, 'r:gz') as tar:
             tar.extractall(tmp_dir)
@@ -17,5 +20,12 @@ def load_collection(filepath, collection):
         db_file = glob.glob("{0}/repeat-*/{1}".format(tmp_dir, DEFAULT_COLLECTIONS_FILE))[0]
         engine = sqlalchemy.create_engine("sqlite:///{0}".format(db_file),
                                           execution_options={"sqlite_raw_colnames": True})
-        dfs = pd.read_sql_table(collection, engine)
-        return dfs
+
+        engine.connect()
+
+        inspector = sqlalchemy.inspect(engine)
+        collections = Collections()
+
+        for table in inspector.get_table_names():
+            setattr(collections, table, pd.read_sql_table(table, engine))
+        return collections
